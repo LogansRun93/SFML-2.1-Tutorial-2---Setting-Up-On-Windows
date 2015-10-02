3f8afc0bc2d90e06b851dee888c4dfed60f49f9a
Erik McGuire <erikmcguire@gmail.com>
2015-10-01 20:57:23 -0500

    Added window and keyboard event support

--- TutorialSFML/TutorialSFML/main.cpp
+++ TutorialSFML/TutorialSFML/main.cpp
@@ -1,4 +1,5 @@
 #include "SFML/Graphics.hpp"
+#include <iostream>
 
 int main()
 {
@@ -6,11 +7,47 @@ int main()
 
 	while ( window.isOpen( ) )
 	{
-		// This is a test comment for git -- Erik
+
+		sf::Event event;
+
+		// Poll the window for events
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+				// The window close button was clicked close the window	
+				case sf::Event::Closed:
+					window.close();
+					break;
+
+				// A key was pressed
+				case sf::Event::KeyPressed:
+					std::cout << "Key has been pressed" << std::endl;
+
+					switch (event.key.code)
+					{
+						case sf::Keyboard::A:
+							std::cout << "A" << std::endl;
+							break;
+					}
+					break;
+				// A key was released
+				case sf::Event::KeyReleased:
+					std::cout << "Key has been released" << std::endl;
+
+					switch (event.key.code)
+					{
+						case sf::Keyboard::J:
+							std::cout << "J has been released" << std::endl;
+							break;
+					}
+
+					break;
+
+			}
+		}
+		
 		window.clear( );
-
-
-
 		window.display( );
 	}
 }
